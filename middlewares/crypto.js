2b3a6d24687a656e67a8dd14e6b9548f4aedf7b9
Eniyalkaruppaiyanilt <eniyalkaruppaiyanilt@gmail.com>
2021-06-16 18:24:39 +0530

    crypto

--- middlewares/crypto.js
+++ middlewares/crypto.js
@@ -1,7 +1,7 @@
 const crypto = require('crypto');
 
 const algorithm = 'aes-256-ctr';
-const secretKey = '';
+const secretKey = 'vOVH6sdmpNWjRRIqCc7rdxs01lwHzfr3';
 const iv = crypto.randomBytes(16);
 
 const encrypt = (text) => {
