07de147b243c78315cf7c4d540adb34247d01490
Eniyalkaruppaiyanilt <eniyalkaruppaiyanilt@gmail.com>
2021-06-28 18:39:04 +0530

    profile

--- superadmin/controller/profile.js
+++ superadmin/controller/profile.js
@@ -1,5 +1,5 @@
 const db = require('../../config/dbconnection');
-const personaldetails=db.personaldetails;
+const registration=db.registration;
 express = require('express');
 const jwt = require('jsonwebtoken');
 const bcrypt = require('bcryptjs');
@@ -22,12 +22,21 @@ router.use(fileUpload({
 app.use(express.static(path.join(__dirname, 'public')));
 router.get('/:id', verifytoken, function (req, res, next) {
     const id = req.params.id;
-    personaldetails.findAll({
+    registration.findAll({
         where: {
-            personaldetailkey: id
+            registrationkey: id
         },
-        attributes: ['firstname', 'lastname', 'emailid', 'gender','dob','maritalstatus'], //object
+        attributes: ['firstname', 'lastname', 'emailid', 'phonenumber','image','role'], //object
     }).then(data => {
+        const img = data[0].image;
+        if (img) {
+          
+            data[0].image = "http://ec2-3-82-204-221.compute-1.amazonaws.com:4003/images/" + img
+        }
+        else {
+           
+            data[0].image = "http://ec2-3-82-204-221.compute-1.amazonaws.com:4003/images/nopicture.png"
+        }
             res.send(data[0]);
             winston.info('/userprofile/id=' + id + 'data' + data);
     })
@@ -41,7 +50,7 @@ router.get('/:id', verifytoken, function (req, res, next) {
 router.put('/:id', verifytoken, function (req, res, next) {
     const id = req.params.id;
     const reg = req.body;
-    personaldetails.findByPk(id)
+    registration.findByPk(id)
         .then(data1 => {
             if (!data1) {
                 var response = CF.getStandardResponse(401, "This userid not found");
@@ -49,8 +58,24 @@ router.put('/:id', verifytoken, function (req, res, next) {
                 next();
             }
             else {
-                personaldetails.update(reg, {
-                    where: { personaldetailkey: id }
+                if (req.files) {
+                    let dept = req.files;
+                    var image1 = dept.image;
+                    if (!image1) {
+                        reg.image = data1.image;
+                    }
+                    else {
+                        fs.unlink('./public/images/' + data1.image,(err) => {
+                            if (err) {
+                            }
+                        });
+                        image1.mv('./public/images/' + id + image1.name);
+                        reg.image = id + image1.name;
+                    }
+
+                }
+                registration.update(reg, {
+                    where: { registrationkey: id }
                 })
                     .then(data => {
                         if (!data) {
